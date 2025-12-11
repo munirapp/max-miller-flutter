b5be999968f127b1f8ec953b332fbd5433f2ba41
munirapp <softwaremakassar@gmail.com>
2025-12-11 18:57:41 +0800

    chore: add required properties in gradient container and styled text class

--- basics/lib/gradient_container.dart
+++ basics/lib/gradient_container.dart
@@ -5,19 +5,42 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Colors.deepPurple, Color.fromARGB(255, 204, 17, 225)],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText()),
+      child: const Center(child: StyledText('Hi')),
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(child: StyledText('Hello')),
+//     );
+//   }
+// }
--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -2,5 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:basics/gradient_container.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Colors.deepPurple,
+          Color.fromARGB(255, 204, 17, 225),
+        ),
+      ),
+    ),
+  );
 }
