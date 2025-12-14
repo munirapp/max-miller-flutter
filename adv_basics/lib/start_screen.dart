07e49edcb6120a6d12243749c4cdfe90f52bfa3a
munirapp <softwaremakassar@gmail.com>
2025-12-14 17:35:10 +0800

    chore: add widget transition/change example

--- adv_basics/lib/start_screen.dart
+++ adv_basics/lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +31,7 @@ class StartScreen extends StatelessWidget {
           SizedBox(height: 30),
           OutlinedButton.icon(
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            onPressed: () {},
+            onPressed: startQuiz,
             icon: Icon(Icons.arrow_right_alt),
             label: const Text("Start Quiz"),
           ),
