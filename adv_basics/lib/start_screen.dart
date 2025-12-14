2b205b3a258002f67a33010425ff5a27f548415d
munirapp <softwaremakassar@gmail.com>
2025-12-14 16:58:26 +0800

    chore: add image transparency example

--- adv_basics/lib/start_screen.dart
+++ adv_basics/lib/start_screen.dart
@@ -9,7 +9,15 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: Color.fromARGB(150, 255, 255, 255),
+          ),
+          // Opacity(
+          //   opacity: 0.5,
+          //   child: Image.asset('assets/images/quiz-logo.png', width: 300),
+          // ),
           const SizedBox(height: 80),
           const Text(
             "Learn Flutter in fun way!",
