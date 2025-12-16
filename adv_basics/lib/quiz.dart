ddb4e7baf75845c76c5c27c00619daabfca0e6b3
munirapp <softwaremakassar@gmail.com>
2025-12-17 01:50:15 +0800

    chore: init quiz list screen and answer screen before exam

--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -1,4 +1,6 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/result_screen.dart';
 import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +14,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
@@ -20,12 +23,26 @@ class _QuizState extends State<Quiz> {
     });
   }
 
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget screenWidget = StartScreen(switchScreen);
 
     if (activeScreen == 'question-screen') {
-      screenWidget = const QuestionsScreen();
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultScreen(choosenAnswers: selectedAnswers);
     }
 
     return MaterialApp(
---
+++ adv_basics/lib/result_screen.dart
@@ -0,0 +1,52 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary.dart';
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({super.key, required this.choosenAnswers});
+
+  final List<String> choosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < choosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': choosenAnswers[i],
+      });
+    }
+
+    return summary;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'You answered  $numCorrectQuestions of $numTotalQuestions questions correctly!',
+            ),
+            SizedBox(height: 30),
+            QuestionsSummary(summaryData: summaryData),
+            SizedBox(height: 30),
+            TextButton(onPressed: () {}, child: Text('Restart Quiz!')),
+          ],
+        ),
+      ),
+    );
+  }
+}
