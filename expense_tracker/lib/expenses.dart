7fe7aeb9c386638124509deb468a1520f7a54c2e
munirapp <softwaremakassar@gmail.com>
2025-12-22 19:33:14 +0800

    chore: add list view handling for long data

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -29,7 +30,12 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [Text("The chart"), Text("Expenses list")]),
+      body: Column(
+        children: [
+          const Text("The chart"),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
     );
   }
 }
